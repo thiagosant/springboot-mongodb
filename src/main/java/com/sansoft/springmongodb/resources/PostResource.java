85a3e0d6eabbd0c95b5fb8247228d3220e1a05ef
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-14 15:24:44 -0300

    Consulta simples com query methods

--- src/main/java/com/sansoft/springmongodb/resources/PostResource.java
+++ src/main/java/com/sansoft/springmongodb/resources/PostResource.java
@@ -2,10 +2,13 @@ package com.sansoft.springmongodb.resources;
 
 
 import com.sansoft.springmongodb.domain.Post;
+import com.sansoft.springmongodb.resources.util.URL;
 import com.sansoft.springmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -16,5 +19,11 @@ public class PostResource {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        return ResponseEntity.ok().body(service.findByTitle(text));
     }
 }
--- src/main/java/com/sansoft/springmongodb/services/PostService.java
+++ src/main/java/com/sansoft/springmongodb/services/PostService.java
@@ -17,4 +17,8 @@ public class PostService {
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
+
 }
