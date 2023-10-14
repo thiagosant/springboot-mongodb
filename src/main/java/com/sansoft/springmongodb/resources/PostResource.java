bec968e3abc31dc48ef583b68cbb15bc5bfb01fd
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-14 15:58:38 -0300

    Consulta com vários critérios

--- src/main/java/com/sansoft/springmongodb/resources/PostResource.java
+++ src/main/java/com/sansoft/springmongodb/resources/PostResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -26,4 +27,17 @@ public class PostResource {
         text = URL.decodeParam(text);
         return ResponseEntity.ok().body(service.findByTitle(text));
     }
+
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate)
+    {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        return ResponseEntity.ok().body(service.fullSearch(text, min, max));
+    }
+
 }
