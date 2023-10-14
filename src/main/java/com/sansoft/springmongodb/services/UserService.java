d73c64132b86bb865dcfc7b793c0f4c9d1f5aea7
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-14 13:25:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/sansoft/springmongodb/services/UserService.java
+++ src/main/java/com/sansoft/springmongodb/services/UserService.java
@@ -1,13 +1,13 @@
 package com.sansoft.springmongodb.services;
 
 import com.sansoft.springmongodb.domain.User;
+import com.sansoft.springmongodb.dto.UserDTO;
 import com.sansoft.springmongodb.repository.UserRepository;
 import com.sansoft.springmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +21,13 @@ public class UserService {
 
     public User findById(String id) {
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
